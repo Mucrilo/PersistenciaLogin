c966aa90357b2d906de7d78d3e74d2150e8b1f38
Murilo Santos <mucrilobass@gmail.com>
2022-04-11 20:00:09 -0400

    Main Criada

--- app/src/main/java/com/example/persistencialogin/DBHelper.java
+++ app/src/main/java/com/example/persistencialogin/DBHelper.java
@@ -1,4 +1,69 @@
 package com.example.persistencialogin;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION=1;
+    private static final String DATABASE_NAME="contato.db";
+    private static final String TABLE_NAME="contato";
+    private static final String COL_NAME="nome";
+    private static final String COL_EMAIL="email";
+    private static final String COL_USUARIO="usuario";
+    private static final String COL_PAS="senha";
+    private static final String TABLE_CREATE="create table " + TABLE_NAME +
+            "("+COL_NAME + " text not null, " + COL_EMAIL + " text not null, " +
+            COL_USUARIO + " text not null, " + COL_PAS + " text not null);";
+    private SQLiteDatabase db;
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+        this.db=db;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String query="DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+    }
+
+    public void insereContato(Contato c){
+        db=this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, c.getNome());
+        values.put(COL_EMAIL, c.getEmail());
+        values.put(COL_USUARIO, c.getUsuario());
+        values.put(COL_PAS, c.getSenha());
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public String buscarSenha(String usuario){
+        db=this.getReadableDatabase();
+        String query="select usuario, senha from " + TABLE_NAME;
+        String a, b;
+        b="não encontrado";
+        Cursor cursor=db.rawQuery(query, null);
+        if (cursor.moveToFirst()){
+            do{
+                a=cursor.getString(0);
+                if (a.equals(usuario)){
+                    b=cursor.getString(1);
+                    break;
+                }
+            }while (cursor.moveToNext());
+        }
+        return b;
+    }
+
 }
--- app/src/main/java/com/example/persistencialogin/MainActivity.java
+++ app/src/main/java/com/example/persistencialogin/MainActivity.java
@@ -2,20 +2,41 @@ package com.example.persistencialogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUsuario, edtSenha;
+    private DBHelper helper = new DBHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtUsuario=findViewById(R.id.edtUsuario);
+        edtSenha=findViewById(R.id.edtSenha);
     }
 
     public void conectar(View view) {
+        String usr=edtUsuario.getText().toString();
+        String senha=edtSenha.getText().toString();
+        String pass=helper.buscarSenha(usr);
+        if (senha.equals(pass)){
+            Intent it=new Intent(MainActivity.this, Activity3.class);
+            it.putExtra("chave_usuario", usr);
+            startActivity(it);
+        }else{
+            Toast toast=Toast.makeText(MainActivity.this,
+                    "Usuário ou senha inválido!", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
     public void cadastrar(View view) {
+        Intent it=new Intent(MainActivity.this, Activity2.class);
+        startActivity(it);
     }
 }
