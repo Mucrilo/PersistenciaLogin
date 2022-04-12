b01c9c6ee92db209b3082f73ecd15de5382b0189
Murilo Santos <mucrilobass@gmail.com>
2022-04-11 20:17:32 -0400

    Atividades Criadas

--- app/src/main/java/com/example/persistencialogin/Activity2.java
+++ app/src/main/java/com/example/persistencialogin/Activity2.java
@@ -4,18 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
+    private EditText edtNome, edtUsur, edtEmail, edtSenha, edtConf;
+    private DBHelper helper = new DBHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        edtNome=findViewById(R.id.edtNome);
+        edtEmail=findViewById(R.id.edtEmail);
+        edtUsur=findViewById(R.id.edtUsuario);
+        edtSenha=findViewById(R.id.edtSenha);
+        edtConf=findViewById(R.id.edtConfSenha);
     }
-
     public void cadastrar(View view) {
+        String nome = edtNome.getText().toString();
+        String email = edtEmail.getText().toString();
+        String usuario = edtUsur.getText().toString();
+        String senha = edtSenha.getText().toString();
+        String confSenha = edtConf.getText().toString();
+        if(!senha.equals(confSenha)){
+            Toast toast = Toast.makeText(Activity2.this,
+                    "Senha diferente da confirmação de senha!",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+            edtSenha.setText("");
+            edtConf.setText("");
+        }
+        else{
+            Contato c = new Contato(nome,email,usuario,senha);
+            helper.insereContato(c);
+            Toast toast = Toast.makeText(Activity2.this,
+                    "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT);
+            toast.show();
+            limpar();
+            finish();
+        }
+    }
+    private void limpar() {
+        edtNome.setText("");
+        edtEmail.setText("");
+        edtUsur.setText("");
+        edtSenha.setText("");
+        edtConf.setText("");
     }
 
     public void cancelar(View view) {
+        finish();
     }
 }
--- app/src/main/java/com/example/persistencialogin/Activity3.java
+++ app/src/main/java/com/example/persistencialogin/Activity3.java
@@ -3,12 +3,19 @@ package com.example.persistencialogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity3 extends AppCompatActivity {
+    private TextView txtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        txtNome = findViewById(R.id.txtNome);
+        Bundle args = getIntent().getExtras();
+        String nome = args.getString("chave_usuario");
+        txtNome.setText("Bem vindo "+nome);
     }
 }
